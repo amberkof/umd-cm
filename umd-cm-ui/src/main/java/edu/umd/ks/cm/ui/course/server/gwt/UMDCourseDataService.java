6477df1824bbe8b17da2b5c456f6f0e8086255e6
cmann <cmann@4eeff667-6e72-6e78-9c18-be2dee0dc652>
2013-01-11 16:40:17 +0000

    KSCM-2155 CM20 upgrade - fix overlay so it compiles without error - course data service
    git-svn-id: https://svn.dev.umd.edu/sds/kuali/umd-cm/branches/umd-cm-2.x-latest@3073 4eeff667-6e72-6e78-9c18-be2dee0dc652

--- umd-cm-ui/src/main/java/edu/umd/ks/cm/ui/course/server/gwt/UMDCourseDataService.java
+++ umd-cm-ui/src/main/java/edu/umd/ks/cm/ui/course/server/gwt/UMDCourseDataService.java
@@ -1,118 +1,24 @@
 package edu.umd.ks.cm.ui.course.server.gwt;
 
 import java.util.ArrayList;
-
-import org.kuali.student.common.ui.client.application.Application;
-import org.kuali.student.common.ui.client.configurable.mvc.FieldDescriptor;
-import org.kuali.student.common.ui.client.configurable.mvc.FieldDescriptorReadOnly;
-import org.kuali.student.common.ui.client.configurable.mvc.SectionTitle;
-import org.kuali.student.common.ui.client.configurable.mvc.binding.ModelWidgetBinding;
-import org.kuali.student.common.ui.client.configurable.mvc.multiplicity.MultiplicityConfiguration;
-import org.kuali.student.common.ui.client.configurable.mvc.multiplicity.MultiplicityFieldConfiguration;
-import org.kuali.student.common.ui.client.configurable.mvc.sections.Section;
-import org.kuali.student.common.ui.client.configurable.mvc.sections.VerticalSection;
-import org.kuali.student.common.ui.client.mvc.Controller;
-import org.kuali.student.common.ui.client.mvc.DataModel;
-import org.kuali.student.common.ui.client.mvc.ModelRequestCallback;
-import org.kuali.student.r1.common.assembly.data.Data;
-import org.kuali.student.r1.common.assembly.data.MetadataInterrogator;
-import org.kuali.student.r1.common.assembly.data.QueryPath;
-import org.kuali.student.r1.common.assembly.data.Data.Property;
-import org.kuali.student.r2.common.dto.ValidationResultInfo;
-import org.kuali.student.r2.common.infc.ValidationResult.ErrorLevel;
-
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.user.client.ui.Widget;
 import java.util.List;
 import java.util.Map;
 
-import org.kuali.rice.kew.dto.DocumentDetailDTO;
-import org.kuali.student.common.exceptions.MissingParameterException;
-import org.kuali.student.common.search.dto.SearchParam;
-import org.kuali.student.common.search.dto.SearchRequestInfo;
-import org.kuali.student.common.search.dto.SearchResult;
-import org.kuali.student.common.search.dto.SearchResultCell;
-import org.kuali.student.common.search.dto.SearchResultRow;
-import org.kuali.student.core.organization.service.OrganizationService;
-import org.kuali.student.core.proposal.dto.ProposalInfo;
-import org.kuali.student.lum.lu.dto.CluInfo;
+import org.kuali.rice.kew.api.document.DocumentDetail;
 import org.kuali.student.lum.lu.ui.course.server.gwt.CourseDataService;
-import org.apache.log4j.Logger;
-import org.kuali.student.common.ui.server.gwt.AbstractDataService;
-import org.kuali.student.core.assembly.transform.ProposalWorkflowFilter;
-import org.kuali.student.r2.common.dto.ContextInfo;
-import org.kuali.student.r2.common.dto.DtoConstants;
-import org.kuali.student.r2.common.dto.ValidationResultInfo;
-import org.kuali.student.r2.common.exceptions.DoesNotExistException;
+import org.kuali.student.r2.common.exceptions.InvalidParameterException;
+import org.kuali.student.r2.common.exceptions.MissingParameterException;
 import org.kuali.student.r2.common.exceptions.OperationFailedException;
-import org.kuali.student.r2.core.search.dto.SearchRequestInfo;
-import org.kuali.student.r2.core.search.dto.SearchResultInfo;
+import org.kuali.student.r2.common.exceptions.PermissionDeniedException;
 import org.kuali.student.r2.common.util.ContextUtils;
-import org.kuali.student.r2.core.versionmanagement.dto.VersionDisplayInfo;
-import org.kuali.student.r2.lum.clu.CLUConstants;
-import org.kuali.student.r2.lum.clu.service.CluService;
-import org.kuali.student.r2.lum.course.dto.CourseCrossListingInfo;
-import org.kuali.student.r2.lum.course.dto.CourseInfo;
-import org.kuali.student.r2.lum.course.service.CourseService;
-import org.kuali.student.r2.lum.lrc.dto.ResultValuesGroupInfo;
-import org.kuali.student.r2.lum.util.constants.CluServiceConstants;
-import org.kuali.student.r2.lum.util.constants.CourseServiceConstants;
-import org.springframework.util.StringUtils;
-
-import org.apache.log4j.Logger;
-import org.kuali.student.common.ui.server.gwt.AbstractDataService;
-import org.kuali.student.core.assembly.transform.ProposalWorkflowFilter;
-import org.kuali.student.r2.common.dto.ContextInfo;
-import org.kuali.student.r2.common.dto.DtoConstants;
-import org.kuali.student.r2.common.dto.ValidationResultInfo;
-import org.kuali.student.r2.common.exceptions.DoesNotExistException;
-import org.kuali.student.r2.common.exceptions.OperationFailedException;
+import org.kuali.student.r2.core.organization.service.OrganizationService;
+import org.kuali.student.r2.core.proposal.dto.ProposalInfo;
+import org.kuali.student.r2.core.search.dto.SearchParamInfo;
 import org.kuali.student.r2.core.search.dto.SearchRequestInfo;
-import org.kuali.student.r2.core.search.dto.SearchResultInfo;
-import org.kuali.student.r2.common.util.ContextUtils;
-import org.kuali.student.r2.core.versionmanagement.dto.VersionDisplayInfo;
-import org.kuali.student.r2.lum.clu.CLUConstants;
-import org.kuali.student.r2.lum.clu.service.CluService;
-import org.kuali.student.r2.lum.course.dto.CourseCrossListingInfo;
-import org.kuali.student.r2.lum.course.dto.CourseInfo;
-import org.kuali.student.r2.lum.course.service.CourseService;
-import org.kuali.student.r2.lum.lrc.dto.ResultValuesGroupInfo;
-import org.kuali.student.r2.lum.util.constants.CluServiceConstants;
-import org.kuali.student.r2.lum.util.constants.CourseServiceConstants;
-import org.springframework.util.StringUtils;
-
-import org.apache.log4j.Logger;
-import org.kuali.student.common.ui.server.gwt.AbstractDataService;
-import org.kuali.student.core.assembly.transform.ProposalWorkflowFilter;
-import org.kuali.student.r2.common.dto.ContextInfo;
-import org.kuali.student.r2.common.dto.DtoConstants;
-import org.kuali.student.r2.common.dto.ValidationResultInfo;
-import org.kuali.student.r2.common.exceptions.DoesNotExistException;
-import org.kuali.student.r2.common.exceptions.OperationFailedException;
-import org.kuali.student.r2.core.search.dto.SearchRequestInfo;
-import org.kuali.student.r2.core.search.dto.SearchResultInfo;
-import org.kuali.student.r2.common.util.ContextUtils;
-import org.kuali.student.r2.core.versionmanagement.dto.VersionDisplayInfo;
-import org.kuali.student.r2.lum.clu.CLUConstants;
-import org.kuali.student.r2.lum.clu.service.CluService;
-import org.kuali.student.r2.lum.course.dto.CourseCrossListingInfo;
-import org.kuali.student.r2.lum.course.dto.CourseInfo;
-import org.kuali.student.r2.lum.course.service.CourseService;
-import org.kuali.student.r2.lum.lrc.dto.ResultValuesGroupInfo;
-import org.kuali.student.r2.lum.util.constants.CluServiceConstants;
-import org.kuali.student.r2.lum.util.constants.CourseServiceConstants;
-import org.springframework.util.StringUtils;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import org.kuali.student.r2.core.search.infc.SearchResult;
+import org.kuali.student.r2.core.search.infc.SearchResultCell;
+import org.kuali.student.r2.core.search.infc.SearchResultRow;
+import org.kuali.student.r2.lum.clu.dto.CluInfo;
 /**
  * Overridden COruse data service
  *
@@ -128,9 +34,10 @@ public class UMDCourseDataService extends CourseDataService {
 	 */
 	@Override
 	protected void addAdditionalAttributes(Map<String, String> attributes,
-			ProposalInfo proposalInfo, DocumentDetailDTO docDetail) {
+			ProposalInfo proposalInfo, DocumentDetail docDetail) {
 		try {
-			CluInfo clu = luService.getClu(proposalInfo.getProposalReference().get(0));
+		   //2.0 upgrade - can we use ContextUtils to get context info here?
+			CluInfo clu = cluService.getClu(proposalInfo.getProposalReference().get(0),ContextUtils.createDefaultContextInfo());
 			if(clu.getAdminOrgs()!=null&&!clu.getAdminOrgs().isEmpty()){
 				String deptId = clu.getAdminOrgs().get(0).getOrgId();
 				attributes.put("department", deptId);
@@ -143,22 +50,22 @@ public class UMDCourseDataService extends CourseDataService {
 	}
 
 	//Use Search to find the college from the department
-	private String getCollegeOrgIdFromDeptId(String deptId) throws MissingParameterException {
+	private String getCollegeOrgIdFromDeptId(String deptId) throws MissingParameterException, InvalidParameterException, OperationFailedException, PermissionDeniedException {
         if (null != deptId) {
-            List<SearchParam> queryParamValues = new ArrayList<SearchParam>(3);
-            SearchParam qpRelType = new SearchParam();
+            List<SearchParamInfo> queryParamValues = new ArrayList<SearchParamInfo>(3);
+            SearchParamInfo qpRelType = new SearchParamInfo();
             qpRelType.setKey("org.queryParam.relationType");
-            qpRelType.setValue("kuali.org.CurriculumParent");
+            qpRelType.getValues().add("kuali.org.CurriculumParent");
             queryParamValues.add(qpRelType);
 
-            SearchParam qpOrgId = new SearchParam();
+            SearchParamInfo qpOrgId = new SearchParamInfo();
             qpOrgId.setKey("org.queryParam.orgId");
-            qpOrgId.setValue(deptId);
+            qpOrgId.getValues().add(deptId);
             queryParamValues.add(qpOrgId);
 
-            SearchParam qpRelOrgType = new SearchParam();
+            SearchParamInfo qpRelOrgType = new SearchParamInfo();
             qpRelOrgType.setKey("org.queryParam.relatedOrgType");
-            qpRelOrgType.setValue("kuali.org.College");
+            qpRelOrgType.getValues().add("kuali.org.College");
             queryParamValues.add(qpRelOrgType);
 
             SearchRequestInfo searchRequest = new SearchRequestInfo();
@@ -166,7 +73,7 @@ public class UMDCourseDataService extends CourseDataService {
             searchRequest.setParams(queryParamValues);
             
             //Perform Search
-            SearchResult results = orgService.search(searchRequest);
+            SearchResult results = orgService.search(searchRequest,ContextUtils.createDefaultContextInfo());
             
             //Find the org ID in the results
             for (SearchResultRow result : results.getRows()) {
